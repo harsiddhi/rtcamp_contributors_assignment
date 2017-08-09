38835a016242efc05123471365a8203a9a512b31
harsiddhi <harsiddhithakkar94@gmail.com>
2017-08-09 14:19:19 +0530

    upload wordpress contributor plugin.

--- includes/class-wordpress-contributors.php
+++ includes/class-wordpress-contributors.php
@@ -1,18 +1,20 @@
 <?php
+
 /**
  * WordPress_Contributors class.
  */
 class WordPress_Contributors {
 	/**
-	 *@access public
+	 * @access public
 	 *
 	 */
 	public function __construct() {
-		
+
 		// Actions
 		add_action( 'add_meta_boxes', array( $this, 'wp_contributors_add_meta_box' ) );
 		add_action( 'save_post', array( $this, 'save' ) );
 	}
+
 	/**
 	 * Adds the meta box container.
 	 *
@@ -23,21 +25,21 @@ class WordPress_Contributors {
 	 * @return void
 	 */
 	public function wp_contributors_add_meta_box( $post_type ) {
-		
-            $post_types = array('post');     //limit meta box to certain post types i.e  'page' or product etc.
-            
-            if ( in_array( $post_type, $post_types )) {
-                add_meta_box(
-                        'post_contributors'
-                        ,__( 'Contributors', 'post_contributors' )
-                        ,array( $this, 'render_meta_box_content' )
-                        ,$post_type
-                        ,'advanced'
-                        ,'high'
+
+		$post_types = array( 'post' );     //limit meta box to certain post types i.e  'page' or product etc.
+
+		if ( in_array( $post_type, $post_types ) ) {
+			add_meta_box(
+				'post_contributors'
+				, __( 'Contributors', 'post_contributors' )
+				, array( $this, 'render_meta_box_content' )
+				, $post_type
+				, 'advanced'
+				, 'high'
 
 
-                );
-            }
+			);
+		}
 	}
 
 	/**
@@ -56,10 +58,10 @@ class WordPress_Contributors {
 
 		// Use get_post_meta to retrieve an existing value from the database.
 		$values = maybe_unserialize( get_post_meta( $post->ID, '_post_contributors_list_meta_value_key', true ) );
-		
+
 		// Get Blog User list as a array
 		/** @noinspection PhpVariableNamingConventionInspection */
-		$users = get_users('role=author');
+		$users = get_users( 'role=author' );
 
 		// Display the form, using the current value.
 		echo '<label for="post_contributors_list">';
@@ -69,28 +71,28 @@ class WordPress_Contributors {
 		echo "<ul>";
 		/** @noinspection PhpUnusedLocalVariableInspection */
 		$selected = '';
-			foreach ( $users as $user_object ) {
-				$user_id   = $user_object->ID;
-				$user_name = $user_object->display_name;
-				
-				
-				if ( is_array($values) && in_array($user_id, $values) ) {
-					$selected = 'checked="checked"';
-				} else {
-					$selected = '';
-				}
-				echo "<li>";
-					echo "<label>";
-						echo "<input type='checkbox' name='post_contributors_list[]' value='".esc_attr( $user_id ) . "' " . $selected . "/>";
-						echo esc_attr( __( $user_name, 'post_contributors' ));
-					echo "</label>";
-				echo "</li>";
-				/** @noinspection PhpUnusedLocalVariableInspection */
+		foreach ( $users as $user_object ) {
+			$user_id   = $user_object->ID;
+			$user_name = $user_object->display_name;
+
+
+			if ( is_array( $values ) && in_array( $user_id, $values ) ) {
+				$selected = 'checked="checked"';
+			} else {
 				$selected = '';
 			}
+			echo "<li>";
+			echo "<label>";
+			echo "<input type='checkbox' name='post_contributors_list[]' value='" . esc_attr( $user_id ) . "' " . $selected . "/>";
+			echo esc_attr( __( $user_name, 'post_contributors' ) );
+			echo "</label>";
+			echo "</li>";
+			/** @noinspection PhpUnusedLocalVariableInspection */
+			$selected = '';
+		}
 		echo "</ul>";
-		
-		
+
+
 	}
 
 	/**
@@ -103,45 +105,51 @@ class WordPress_Contributors {
 	 * @return int $post_id
 	 */
 	public function save( $post_id ) {
-		
+
 		/*
 		 * We need to verify this came from the our screen and with proper authorization,
 		 * because save_post can be triggered at other times.
 		 */
 
 		// Check if our nonce is set.
-		if ( ! isset( $_POST['post_contributors_box_nonce'] ) )
+		if ( ! isset( $_POST['post_contributors_box_nonce'] ) ) {
 			return $post_id;
+		}
 
 		$nonce = $_POST['post_contributors_box_nonce'];
 
 		// Verify that the nonce is valid.
-		if ( ! wp_verify_nonce( $nonce, 'post_contributors_box' ) )
+		if ( ! wp_verify_nonce( $nonce, 'post_contributors_box' ) ) {
 			return $post_id;
+		}
 
 		// If this is an save, our form has not been submitted,
-                //     so we don't want to do anything.
-		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) 
+		//     so we don't want to do anything.
+		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
 			return $post_id;
+		}
 
 		// Check the user's permissions.
 		if ( 'page' == $_POST['post_type'] ) {
 
-			if ( ! current_user_can( 'edit_page', $post_id ) )
+			if ( ! current_user_can( 'edit_page', $post_id ) ) {
 				return $post_id;
-	
+			}
+
 		} else {
 
-			if ( ! current_user_can( 'edit_post', $post_id ) )
+			if ( ! current_user_can( 'edit_post', $post_id ) ) {
 				return $post_id;
+			}
 		}
 
-		
+
 		// Sanitize the user input.
 		$contributors_list = sanitize_text_field( serialize( $_POST['post_contributors_list'] ) );
-		
+
 		// Update the meta field.
 		update_post_meta( $post_id, '_post_contributors_list_meta_value_key', $contributors_list );
+
 		return $post_id;
 	}
 }
--- wordpress-contributors.php
+++ wordpress-contributors.php
@@ -1,98 +1,92 @@
 <?php
 /**
-*Plugin Name: Wordpress Contributors
-*Description: This plugin allow to set more than one post author in wordpress post.
-*Author: Harsiddhi Thakkar
-*Author Url:https://github.com/harsiddhi
-*Version: 1.0
-* License:GPL-2.0+
-*/
+ *Plugin Name: Wordpress Contributors
+ *Description: This plugin allow to set more than one post author in wordpress post.
+ *Author: Harsiddhi Thakkar
+ *Author Url:https://github.com/harsiddhi
+ *Version: 1.0
+ * License:GPL-2.0+
+ */
 
 /**
  * Add wp-contributors the class .
  */
 require_once 'includes/class-wordpress-contributors.php';
-
-
 /**
  * Calls the class on the post edit screen.
  */
 function call_wp_post_contributors() {
-    new WordPress_Contributors();
+	new WordPress_Contributors();
 }
 
 /**
  * Calls the function (only when in admin area)
  */
 if ( is_admin() ) {
-    add_action( 'load-post.php', 'call_wp_post_contributors' );
-    add_action( 'load-post-new.php', 'call_wp_post_contributors' );
+	add_action( 'load-post.php', 'call_wp_post_contributors' );
+	add_action( 'load-post-new.php', 'call_wp_post_contributors' );
 }
-
-
 /**
  * Calls the function  (only when not in admin area)
  */
-if ( !is_admin() ) {
-    add_filter( 'the_content', 'wp_contributors_post_content_meta_box' );
+if ( ! is_admin() ) {
+	add_filter( 'the_content', 'wp_contributors_post_content_meta_box' );
 }
-
 /**
  * Display post_contributors below post content.
  *
  * @param  object $content
+ *
  * @return object $content
  */
 function wp_contributors_post_content_meta_box( $content ) {
-	
+
 	global $post;
-	
+
 	// Use get_post_meta to retrieve an existing value from the database.
 	$post_contributors = maybe_unserialize( get_post_meta( $post->ID, '_post_contributors_list_meta_value_key', true ) );
-	
-	if (!empty($post_contributors)) {
-			
+
+	if ( ! empty( $post_contributors ) ) {
+
 		$contributors_box = '<h3 class="post-contributors-box">';
 		$contributors_box .= __( 'Contributors', 'post_contributors' );
 		$contributors_box .= '</h3>';
 		$contributors_box .= '<ul class="post-contributors-box">';
-		
+
 		foreach ( $post_contributors as $post_contributor ) {
-				
-			$contributors_name = get_the_author_meta('user_login', $post_contributor);
-			
+
+			$contributors_name = get_the_author_meta( 'user_login', $post_contributor );
+
 			// if user is there
 			if ( $contributors_name ) {
-				
+
 				$contributors_url    = get_author_posts_url( $post_contributor, $contributors_name );
 				$contributors_avatar = get_avatar( $post_contributor, 24, '', 'avatar' );
 				$contributors_count  = count_user_posts( $post_contributor ); // get the post count
-				
+
 				$contributors_box .= '<li>';
 				$contributors_box .= $contributors_avatar;
-				
+
 				// if post count > 0 , add link.
-				if ( $contributors_count > 0) {
+				if ( $contributors_count > 0 ) {
 					$contributors_box .= '<a href="' . $contributors_url . '" rel="author" target="_blank">';
 				}
 
 				$contributors_box .= '<span>';
 				$contributors_box .= $contributors_name;
 				$contributors_box .= '</span>';
-				
+
 				// if post count > 0 , close link.
-				if ( $contributors_count > 0) {
+				if ( $contributors_count > 0 ) {
 					$contributors_box .= '</a>';
 				}
-				
+
 				$contributors_box .= '</li>';
 			}
-			
 		}
-		
+
 		$contributors_box .= '</ul>';
-		
-		$content = $content . $contributors_box;
+		$content          = $content . $contributors_box;
 	}
 
 	return $content;
